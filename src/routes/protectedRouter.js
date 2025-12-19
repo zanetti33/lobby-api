19b285f42b1b19a96b9a1035ff6adab97fbea521
Alessandro Zanzi <alessandro.zanzi2@studio.unibo.it>
2025-12-19 12:44:07 +0100

    feat: added api for create/delete a room, add/remove a player to a room

--- src/routes/protectedRouter.js
+++ src/routes/protectedRouter.js
@@ -5,4 +5,19 @@ const controller = require('../controllers/roomController');
 router.route('/rooms')
     .get(controller.listRooms);
 
+router.route('/rooms')
+    .post(controller.createRoom);
+
+router.route('/rooms/:id')
+    .get(controller.getRoom);
+
+router.route('/rooms/room/addPlayer')
+    .put(controller.addPlayer);
+
+router.route('/rooms/room/removePlayer')
+    .put(controller.removePlayer);
+
+router.route('/rooms/room/delete')
+    .delete(controller.deleteRoom);
+
 module.exports = router;
