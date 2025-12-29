bf20ee533ee05fd0540ebd8dc6404e08ea615c57
Fabio Pedrini <fabio.pedrini3@studio.unibo.it>
2025-12-29 15:13:19 +0100

    chore: refactored routes

--- src/routes/protectedRouter.js
+++ src/routes/protectedRouter.js
@@ -3,27 +3,21 @@ const router = express.Router();
 const controller = require('../controllers/roomController');
 
 router.route('/rooms')
-    .get(controller.listRooms);
-
-router.route('/rooms')
+    .get(controller.listRooms)
     .post(controller.createRoom);
 
 router.route('/rooms/search/:codeOrName')
     .get(controller.getRoomByCodeOrName);
 
 router.route('/rooms/:id')
-    .get(controller.getRoom);
+    .get(controller.getRoom)
+    .delete(controller.deleteRoom);
 
 router.route('/rooms/:id/players')
-    .post(controller.addPlayer);
-
-router.route('/rooms/:id/players')
+    .post(controller.addPlayer)
     .put(controller.isReady);
 
 router.route('/rooms/:id/players/:userId')
     .delete(controller.removePlayer);
-
-router.route('/rooms/:id')
-    .delete(controller.deleteRoom);
 
 module.exports = router;
